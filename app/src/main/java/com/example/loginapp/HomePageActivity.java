5dc4fdcae1e94b8c988c1c955fa22a1e31368953
ktan162 <70625254+ktan162@users.noreply.github.com>
2020-09-09 17:08:19 +0800

    Added Recommended Curriculum Structure Feature

--- app/src/main/java/com/example/loginapp/HomePageActivity.java
+++ app/src/main/java/com/example/loginapp/HomePageActivity.java
@@ -2,7 +2,10 @@ package com.example.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+    public void csbrowser(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.eee.ntu.edu.sg/Programmes/ProspectiveStudents/UG/IEM/Curriculum/Pages/Curriculum-Structure.aspx"));
+        startActivity(browserIntent);
     }
 }
