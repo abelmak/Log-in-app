f56afdbe85bf2e651408a4f2e7c614f7404989e5
abelmak <70623978+abelmak@users.noreply.github.com>
2020-09-23 13:52:38 +0800

    Added registration activity

---
+++ app/src/main/java/com/example/loginapp/RegistrationActivity.java
@@ -0,0 +1,51 @@
+package com.example.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegistrationActivity extends AppCompatActivity {
+    //create more variables here
+    private EditText eRegName;
+    private EditText eRegPassword;
+    private Button eRegister;
+
+    public static Credentials credentials;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        eRegName = findViewById(R.id.etRegName);
+        eRegPassword = findViewById(R.id.etRegPassword);
+        eRegister = findViewById(R.id.btnRegister);
+
+        eRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String regUsername = eRegName.getText().toString();
+                String regPassword = eRegPassword.getText().toString();
+
+                if(validate(regUsername, regPassword)){
+                    credentials = new Credentials(regUsername, regPassword);
+                    startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                    Toast.makeText(RegistrationActivity.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+    private boolean validate(String username, String password){
+        if(username.isEmpty() || password.length() < 8){
+            Toast.makeText(this, "Please enter all the details, password should be at least 8 characters!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+}
