ac678028387c4255b06179d547179be0416bc48f
ktan162 <70625254+ktan162@users.noreply.github.com>
2020-10-21 15:35:11 +0800

    Update GPA Calculator 2.0

--- app/src/main/java/com/example/loginapp/GPACalculator.java
+++ app/src/main/java/com/example/loginapp/GPACalculator.java
@@ -43,21 +43,47 @@ public class GPACalculator extends AppCompatActivity{
             public void onClick(View v) {
                 float g1, g2, g3, g4, g5, h1, h2, h3, h4, h5;
                 if (count % 2 == 0) {
-                    if (e1.getText().toString().equals("") || e2.getText().toString().equals("") || e3.getText().toString().equals("") || e4.getText().toString().equals("") || e5.getText().toString().equals("")) {
+                    if (e1.getText().toString().equals("") && e2.getText().toString().equals("") && e3.getText().toString().equals("") && e4.getText().toString().equals("") && e5.getText().toString().equals("")) {
                         tViewWarning.setText("Please Enter your Grades");
                         tViewWarning.setTextColor(Color.rgb(255,0,0));
 
                     } else {
+                        if (e1.getText().toString().equals("") || f1.getText().toString().equals(""))
+                        {
+                            g1 = 0; h1 = 0;}
+                            else {
                         g1 = Float.parseFloat(e1.getText().toString());
-                        g2 = Float.parseFloat(e2.getText().toString());
-                        g3 = Float.parseFloat(e3.getText().toString());
-                        g4 = Float.parseFloat(e4.getText().toString());
-                        g5 = Float.parseFloat(e5.getText().toString());
-                        h1 = Float.parseFloat(f1.getText().toString());
-                        h2 = Float.parseFloat(f2.getText().toString());
-                        h3 = Float.parseFloat(f3.getText().toString());
-                        h4 = Float.parseFloat(f4.getText().toString());
-                        h5 = Float.parseFloat(f5.getText().toString());
+                        h1 = Float.parseFloat(f1.getText().toString());}
+
+                        if (e2.getText().toString().equals("") || f2.getText().toString().equals(""))
+                        {
+                            g2 = 0; h2 = 0;}
+                        else {
+                            g2 = Float.parseFloat(e2.getText().toString());
+                            h2 = Float.parseFloat(f2.getText().toString());}
+
+                        if (e3.getText().toString().equals("") || f3.getText().toString().equals(""))
+                        {
+                            g3 = 0; h3 = 0;}
+                        else {
+                            g3 = Float.parseFloat(e3.getText().toString());
+                            h3 = Float.parseFloat(f3.getText().toString());}
+
+                        if (e4.getText().toString().equals("") || f4.getText().toString().equals(""))
+                        {
+                            g4 = 0; h4 = 0;}
+                        else {
+                            g4 = Float.parseFloat(e4.getText().toString());
+                            h4 = Float.parseFloat(f4.getText().toString());}
+
+                        if (e5.getText().toString().equals("") || f5.getText().toString().equals(""))
+                        {
+                            g5 = 0; h5 = 0;}
+                        else {
+                            g5 = Float.parseFloat(e5.getText().toString());
+                            h5 = Float.parseFloat(f5.getText().toString());}
+
+
                         float average = g1*h1 + g2*h2 + g3*h3 + g4*h4 + g5*h5;
                         average = average / (h1 + h2 + h3 + h4 + h5);
                         if (average <= 3.5) {
@@ -81,6 +107,12 @@ public class GPACalculator extends AppCompatActivity{
                     e3.setText("");
                     e4.setText("");
                     e5.setText("");
+                    f1.setText("");
+                    f2.setText("");
+                    f3.setText("");
+                    f4.setText("");
+                    f5.setText("");
+
                     tView.setText("");
                     view.setBackgroundResource(R.color.blue);
                     btn.setText("Compute GPA");
